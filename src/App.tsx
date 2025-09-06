26d4bbab6d8d453a8244e8c9689eb6106d400088
Sheonna Harris <svonharris@gmail.com>
2025-09-06 15:05:02 -0400

    Added header, checkout, and food ordering sections. Wrote HTML for hero section. Expanded button to include icon prop and icon lookup array.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import Button from "./components/Button/Button.tsx";
+// import { PiArrowBendRightDownFill } from "react-icons/pi";
+
 import "./index.css";
 
 function App() {
   return (
     <>
+      <header className="h-10">
+        <div className="logoBox bg-purple-600 h-40 w-30 absolute top-0"></div>
+      </header>
       <main>
         {/* hero */}
-        <section>
-          <h1>Slurp</h1>
-          <Button title={"Order Now"} />
+        <section className="relative h-[60vh]">
+          <h1 className="text-center">Slurp</h1>
+          <p className="text-center max-w-[500px] mt-10 mx-auto">
+            Bacon ipsum dolor amet venison jowl pancetta ground round. Kielbasa
+            fatback salami tenderloin pork, sausage andouille ham pastrami
+            shankle rump ground round meatball.
+          </p>
+          <div className="bg-gray-400 h-[300px] w-[500px] absolute right-0">
+            image of food
+          </div>
+          <div className="absolute bottom-1">
+            <Button title={"Order Now"} icon={"arrowBendRight"} />
+          </div>
         </section>
         {/* ordering */}
         <section className="columns-2 gap-8">
-          <div>description and image</div>
-          <div>customization tabs</div>
+          {/* <div>description and image</div> */}
+          {/* <div>customization tabs</div> */}
         </section>
         {/* checkout */}
         <section></section>
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,37 @@
 // import React from 'react'
+import { PiArrowBendRightDownFill } from "react-icons/pi";
+import { PiArrowBendDownRightFill } from "react-icons/pi";
 import "./Button.css";
 
 type ButtonProps = {
   title: string;
+  icon: string;
 };
 
-const Button = ({ title }: ButtonProps) => {
-  return <button className="btn">{title}</button>;
+const icons = [
+  {
+    name: "arrowBendDown",
+    icon: <PiArrowBendRightDownFill />,
+  },
+  {
+    name: "arrowBendRight",
+    icon: <PiArrowBendDownRightFill />,
+  },
+];
+
+const Button = ({ title, icon }: ButtonProps) => {
+  const foundIcon = icons.find((item) => item.name === icon);
+
+  return (
+    <button className="btn flex flex-row bg-amber-500 p-2 rounded-sm">
+      {title}
+      {foundIcon ? (
+        <span style={{ marginTop: "10px", marginLeft: "3px" }}>
+          {foundIcon.icon}
+        </span>
+      ) : null}
+    </button>
+  );
 };
 
 export default Button;
