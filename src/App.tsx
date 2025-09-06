665955523bd5f5552f4f0438c3afe6df5eebc286
Sheonna Harris <svonharris@gmail.com>
2025-09-06 12:48:40 -0400

    Setting up outline of App.

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import "./index.css";
 function App() {
   return (
     <>
-      <h1>hello world</h1>
-      <Button title={"Order Now"} />
+      <main>
+        {/* hero */}
+        <section>
+          <h1>Slurp</h1>
+          <Button title={"Order Now"} />
+        </section>
+        {/* ordering */}
+        <section className="columns-2 gap-8">
+          <div>description and image</div>
+          <div>customization tabs</div>
+        </section>
+        {/* checkout */}
+        <section></section>
+      </main>
     </>
   );
 }
