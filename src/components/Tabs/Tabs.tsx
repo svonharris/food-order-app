0f9e0fcb2ab6c7c8a6d3306d496b81db33eb7aee
Sheonna Harris <svonharris@gmail.com>
2025-09-08 00:04:44 -0400

    Added to content Tab.

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -32,10 +32,20 @@ const Tabs = () => {
           className={"hidden py-[6px] px-[12px]"}
           style={{ display: activeTab === data.name ? "block" : "none" }}
         >
+          <div className="inline-block bg-white p-[20px] rounded-full">
+            {/* {<data.icon size={30} className="fill-purple-600" />} */}
+            {data.icon}
+          </div>
           <h3>{data.title}</h3>
           <p>{data.content}</p>
+          <p className="block text-xs">+ ${data.price}.00</p>
         </div>
       ))}
+      {/* <div className="flex flex-row gap-[10px]">
+        <Tab />
+        <Tab />
+        <Tab />
+      </div> */}
     </div>
   );
 };
