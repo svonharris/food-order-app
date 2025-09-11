d8f575e13d05a6bfccefc8a1e6a8d5fa2be44ba3
Sheonna Harris <svonharris@gmail.com>
2025-09-11 16:39:25 -0400

    Changed Data import name, started writing prop types, changed activeTab base value, added a new button for viewing all, added new prop for Tab component, added conditionals for Tab component styling.

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,41 +1,73 @@
 import { useState } from "react";
-import { testingData } from "../../Data/Data.tsx";
+import { foodData } from "../../Data/Data.tsx";
 import Tab from "../Tab/Tab.tsx";
 
+// type TabsProps = {
+//   data: {
+//     id: number;
+//     category: string;
+//     ingredients: {}[];
+//   };
+// };
+
 const Tabs = () => {
-  const [activeTab, setActiveTab] = useState("Tab 1");
+  const [activeTab, setActiveTab] = useState("Base");
 
   const handleTabClick = (tabName: string) => {
     setActiveTab(tabName);
   };
 
   return (
-    <div className="w-[100%]">
-      {/* Tab Buttons Group */}
-      <div className="flex gap-4">
-        {testingData.map((data) => (
+    <>
+      <div className="w-[100%]">
+        {/* Tab Buttons Group */}
+        <div className="flex gap-4">
+          {foodData.map((data) => (
+            <button
+              key={data.id}
+              className={`cursor-pointer outline-purple-600 py-[14px] px-[16px] border-[1px] border-solid border-white hover:bg-purple-600 ${
+                activeTab === data.category ? "bg-purple-600" : ""
+              }`}
+              onClick={() => handleTabClick(data.category)}
+            >
+              {data.category}
+            </button>
+          ))}
           <button
-            key={data.id}
             className={`cursor-pointer outline-purple-600 py-[14px] px-[16px] border-[1px] border-solid border-white hover:bg-purple-600 ${
-              activeTab === data.name ? "bg-purple-600" : ""
+              activeTab === "ViewAll" ? "bg-purple-600" : ""
             }`}
-            onClick={() => handleTabClick(data.name)}
+            onClick={() => {
+              handleTabClick("ViewAll");
+            }}
           >
-            {data.name}
+            View All
           </button>
+        </div>
+
+        {/* Tab Content */}
+        {foodData.map((data) => (
+          <div
+            key={data.id}
+            className={`${
+              activeTab === data.category || activeTab === "ViewAll"
+                ? "flex"
+                : "hidden"
+            } ${
+              activeTab === "ViewAll"
+                ? "border-b border-solid border-b-white py-5"
+                : ""
+            }`}
+          >
+            <Tab
+              title={data.category}
+              ingredients={data.ingredients}
+              showTitle={activeTab === "ViewAll" ? true : false}
+            />
+          </div>
         ))}
       </div>
-
-      {/* Tab Content */}
-      {testingData.map((data) => (
-        <div
-          key={data.id}
-          style={{ display: activeTab === data.name ? "flex" : "none" }}
-        >
-          <Tab title={data.title ?? ""} ingredients={data.ingredients} />
-        </div>
-      ))}
-    </div>
+    </>
   );
 };
 
