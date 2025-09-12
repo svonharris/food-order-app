f818e87ab7dc4b60dd6a7de627a2e2778d0f2a5d
Sheonna Harris <svonharris@gmail.com>
2025-09-11 22:25:46 -0400

    Removed React import, changed styles.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,8 @@
-// import React from 'react'
-
 const Header = () => {
   return (
     <>
-      <header>
-        <div className="logoBox bg-purple-600 h-40 w-30 absolute top-0 left-[5%]"></div>
+      <header className="max-w-[1400px] mx-auto my-0">
+        <div className="logoBox bg-purple-600 h-40 w-30"></div>
       </header>
     </>
   );
